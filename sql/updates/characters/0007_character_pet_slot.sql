3d373cceffb6b346cb415bc81d11a9633b6f67f7
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-10-07 11:53:39 -0500

    delete warlock pets

--- sql/updates/characters/0007_character_pet_slot.sql
+++ sql/updates/characters/0007_character_pet_slot.sql
@@ -2,6 +2,9 @@ ALTER TABLE `character_pet` CHANGE `slot` `slot` smallint NOT NULL DEFAULT '-1';
 
 UPDATE `character_pet` SET `slot` = -1 WHERE `slot` = 100;
 
+-- delete all warlock pets to remove duplicate entries from previous bugs
+DELETE FROM `character_pet` WHERE `owner` IN (SELECT `guid` FROM `characters` WHERE `class` = 9);
+
 ALTER TABLE `characters` DROP `stable_slots`;
 
 ALTER TABLE `characters` ADD `summonedPetNumber` int unsigned NOT NULL DEFAULT '0' AFTER `extra_flags`;
