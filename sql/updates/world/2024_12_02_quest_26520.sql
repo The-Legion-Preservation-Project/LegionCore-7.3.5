8368250621269bb78f1b6a923c6e12c851ac7198
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2024-12-03 23:38:18 -0600

    remove old smart script entries

--- sql/updates/world/2024_12_02_quest_26520.sql
+++ sql/updates/world/2024_12_02_quest_26520.sql
@@ -38,6 +38,10 @@ UPDATE `creature_template` SET `ScriptName` = 'npc_redridge_huge_boulder' WHERE 
 UPDATE `creature_template` SET `ScriptName` = 'npc_redridge_subdued_ettin' WHERE `entry` = 43197;
 UPDATE `creature_template` SET `VehicleId` = 938 WHERE `entry` = 43197;
 
+-- clean out old data
+DELETE FROM `smart_scripts` WHERE `entryorguid` IN (341, 648, 649, 650, 651, 652, 653, 43196, 43197) AND `source_type` = 0;
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 4319600 AND `source_type` = 9;
+
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 43094;
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 43094 AND `source_type` = 0;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
